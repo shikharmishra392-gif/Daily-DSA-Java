b1edec7b1581476d8a198a056d51b4244b3383f5
Shikhar Mishra <shikharmishra392@gmail.com>
2026-06-29 14:33:01 +0530

    solved leetcode 189-rotate array k times

--- src/Array/ArrayManipulatonProblem.java
+++ src/Array/ArrayManipulatonProblem.java
@@ -178,36 +178,7 @@ public class ArrayManipulatonProblem {
 //            int k=3;
 //            rotate(nums,k);
 //        }
-    public void reverse(int i,int j,int[]nums){
-       int n= nums.length-1;
-        while(i<j) {
-            int temp = 0;
-            temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
-        }
 
-    }
-    public void rotate(int[] nums, int k){
-        int n= nums.length-1;
-        reverse(0,k,nums);
-        reverse((k+1),n,nums);
-        reverse(0,n,nums);
-
-
-
-
-    }
-    public  void main(String[] args){
-            int[]nums={1,2,3,4,5,6,7};
-            int k=3;
-           rotate(nums,k);
-        for(int val:nums){
-            System.out.println(val);
-        }
-            }
     }
 
 
